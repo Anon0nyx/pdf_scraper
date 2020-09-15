55274ac6ab1f756bf2123526bf886ed84890f76a
anon0nyx <anon0nyx@FileSharingServer.GLOBAL>
2020-09-15 00:34:58 -0400

    Checking the data more than before

--- oop_solution/main_file.py
+++ oop_solution/main_file.py
@@ -5,10 +5,10 @@ COLUMNS = [ 'Names', 'Country', 'Sponsor', 'Observed', 'Tools used' ]
 def main():
 	print('Cleaning Data')
 	library_file.clean_data()
-	time.sleep(3)
 
 	pdf_file = open('./apt_list.txt')
 	count = 0
+	print('Creating CSV file -> output.csv')
 	with open('output.csv', 'w') as out_file:	
 		writer = csv.DictWriter(out_file, fieldnames=COLUMNS)
 		writer.writeheader()
@@ -26,18 +26,20 @@ def main():
 						foobar.append(item)
 				
 				for i in range(len(foobar)):
-					if re.match('Names', str(foobar[i])):
+					if re.match('Names', str(foobar[i])) and not re.match('Country', str(foobar[i+1])):
 						threat_actor.set_names(str(foobar[i+1]))
-					if re.match('Country', str(foobar[i])):
+					if re.match('Country', str(foobar[i])) and not re.match('Sponsor', str(foobar[i+1])):
 						threat_actor.set_country(str(foobar[i+1]))
-					if re.match('Sponsor', str(foobar[i])):
+					if re.match('Sponsor', str(foobar[i])) and not re.match('Observed', str(foobar[i+1])):
 						threat_actor.set_sponsor(str(foobar[i+1]))
-					if re.match('Observed', str(foobar[i])):
+					if re.match('Observed', str(foobar[i])) and not re.match('Tools used', str(foobar[i+1])):
 						threat_actor.set_observed(str(foobar[i+1]))
 					if re.match('Tools used', str(foobar[i])):
 						threat_actor.set_tools(str(foobar[i+1]))
 				writer.writerow({'Names': str(threat_actor.get_names()), 'Country': str(threat_actor.get_country()), 'Sponsor': str(threat_actor.get_sponsor()), 'Observed': str(threat_actor.get_observed()), 'Tools used': str(threat_actor.get_tools()) }) 
 			count -= -1
+	out_file.close()
+	print('PROCESS COMPLETE')
 
 if __name__ == '__main__':
 	main()
